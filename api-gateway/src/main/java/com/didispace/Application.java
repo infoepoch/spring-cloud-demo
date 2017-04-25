809dea89ae0a99c85f837f1075ea48488efb1630
iceinto <iceinto@163.com>
2017-04-25 14:20:10 +0800

    代码初始化

---
+++ api-gateway/src/main/java/com/didispace/Application.java
@@ -0,0 +1,27 @@
+package com.didispace;
+
+import com.didispace.filter.AccessFilter;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableHystrix
+@EnableHystrixDashboard
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
+
+}
