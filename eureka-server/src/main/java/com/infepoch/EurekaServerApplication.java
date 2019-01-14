72131aa07672aa57f4812153bac25a8c95db357d
iceinto <iceinto@163.com>
2019-01-14 17:55:31 +0800

    <优化><项目><名称、引用版本更新：Finchley.SR2>

--- eureka-server/src/main/java/com/infepoch/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/infepoch/EurekaServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
-public class Application {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
